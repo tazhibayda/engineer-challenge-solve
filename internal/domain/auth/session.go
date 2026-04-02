e1b6ab078d30c17308cc910517525f4dc06a6b2c
tazhibayda <tazhibayda@gmail.com>
2026-04-02 18:26:08 +0500

    define session entities

--- internal/domain/auth/session.go
+++ internal/domain/auth/session.go
@@ -1,1 +1,69 @@
 package auth
+
+import (
+	"errors"
+	"github.com/google/uuid"
+	"time"
+)
+
+var (
+	ErrSessionExpired = errors.New("session is expired")
+	ErrSessionRevoked = errors.New("session is revoked")
+)
+
+type Session struct {
+	ID        uuid.UUID
+	UserID    uuid.UUID
+	FamilyID  uuid.UUID
+	ClientIP  string
+	UserAgent string
+	IsRevoked bool
+	ExpiresAt time.Time
+	CreatedAt time.Time
+}
+
+func NewSession(userID, familyID uuid.UUID, clientIP, userAgent string, duriation time.Duration) *Session {
+	now := time.Now().UTC()
+	sessionId := uuid.New()
+
+	return &Session{
+		ID:        sessionId,
+		UserID:    userID,
+		FamilyID:  familyID,
+		ClientIP:  clientIP,
+		UserAgent: userAgent,
+		IsRevoked: false,
+		ExpiresAt: now.Add(duriation),
+		CreatedAt: now,
+	}
+}
+
+func (s *Session) Rotate(ip, userAgent string, duration time.Duration) (*Session, error) {
+	if err := s.CheckValidity(); err != nil {
+		return nil, err
+	}
+	s.IsRevoked = true
+
+	now := time.Now().UTC()
+	return &Session{
+		ID:        uuid.New(),
+		UserID:    s.UserID,
+		FamilyID:  s.FamilyID,
+		ClientIP:  ip,
+		UserAgent: userAgent,
+		IsRevoked: false,
+		ExpiresAt: now.Add(duration),
+		CreatedAt: now,
+	}, nil
+
+}
+
+func (s *Session) CheckValidity() error {
+	if s.IsRevoked {
+		return ErrSessionRevoked
+	}
+	if time.Now().UTC().After(s.ExpiresAt) {
+		return ErrSessionExpired
+	}
+	return nil
+}
